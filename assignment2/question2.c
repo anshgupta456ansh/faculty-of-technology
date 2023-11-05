2b4fbdadd05578f8088935a9ac68e25dc679eaad
anshgupta456ansh <anshgupta456ansh@gmail.com>
2023-11-05 19:47:03 +0530

    assignment 2

--- assignment2/question2.c
+++ assignment2/question2.c
@@ -1,6 +1,20 @@
+//wap two calculate adjoint of a matrix
 #include<stdio.h>
 
 int main(void)
 {
-    
+    int a[3][3],b[3][3];
+    printf("enter values for first matrix\n");
+    for(int l=0; l<3;l ++)
+    {
+        for(int m=0;m<3;m++)
+        {
+            printf("Enter Element at %d%d position: ",l+1,m+1);
+            scanf("%d",&a[l][m]);
+        }
+    }
+    b[0][0]=(a[1][1]*a[2][2])-(a[1][2]*a[2][1]);
+    b[0][1]=-((a[1][0]*a[2][2])-(a[1][2]*a[2][0]));
+    b[0][2]=(a[1][0]*a[2][1])-(a[1][1]*a[2][0]);
+     
 }
--- assignment2/question3.c
+++ assignment2/question3.c
@@ -1,3 +1,4 @@
+//wap to find the inverse of a matrix
 #include<stdio.h>
 
 int main(void)
